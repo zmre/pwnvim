d1509f074957722976e6b7b4b8b363cc6a3aee8d
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2022-10-16 21:44:43 -0600

    initial setup

--- flake.nix
+++ flake.nix
@@ -20,20 +20,123 @@
           withRuby = false;
           extraPython3Packages = false;
           configure = {
-            customRC = "";
-          };
-          packages.myPlugins = with pkgs.vimPlugins; {
-            start = with pkgs.vimPlugins; [onedarkpro-nvim telescope-nvim vim-fugitive ];
-            opt = with pkgs.vimPlugins; [trouble-nvim ];
+            customRC = ''
+              lua << EOF
+                package.path = package.path .. ";${self}"
+                require('zmre.options').defaults()
+                require('zmre.options').gui()
+                require('zmre.mappings')
+                require('zmre.abbreviations')
+                require('zmre.filetypes').config()
+                require('zmre.plugins').ui()
+                require('zmre.plugins').diagnostics()
+                require('zmre.plugins').telescope()
+                require('zmre.plugins').completions()
+                require('zmre.plugins').notes()
+                require('zmre.plugins').misc()
+              EOF
+            '';
+            packages.myPlugins = with pkgs.vimPlugins; {
+              start = with pkgs.vimPlugins; [
+                # Common dependencies of other plugins
+                popup-nvim # dependency of some other plugins
+                plenary-nvim # Library for lua plugins; used by many plugins here
+
+                # Syntax / Language Support ##########################
+                vim-polyglot # lazy load all the syntax plugins for all the languages
+                rust-tools-nvim # lsp stuff and more for rust
+                crates-nvim # inline intelligence for Cargo.toml
+                nvim-lspconfig # setup LSP for intelligent coding
+                # nvim-lsp-ts-utils for inlays
+                null-ls-nvim # formatting and linting via lsp system
+                trouble-nvim # navigate all warnings and errors in quickfix-like window
+                lspsaga-nvim
+                todo-comments-nvim
+                #copilot-vim # github copilot
+
+                # UI #################################################
+                onedarkpro-nvim # colorscheme
+                #onedark-vim # colorscheme
+                zephyr-nvim # alternate colorscheme
+                telescope-nvim # da best popup fuzzy finder
+                telescope-fzy-native-nvim # but fzy gives better results
+                telescope-frecency-nvim # and frecency comes in handy too
+                dressing-nvim # dresses up vim.ui.input and vim.ui.select and uses telescope
+                nvim-colorizer-lua # color over CSS like #00ff00
+                nvim-web-devicons # makes things pretty; used by many plugins below
+                nvim-tree-lua # file navigator
+                gitsigns-nvim # git status in gutter
+                symbols-outline-nvim # navigate the current file better
+                lualine-nvim # nice status bar at bottom
+                #barbar-nvim # nice buffers (tabs) bar at top
+                vim-bbye # fix bdelete buffer stuff needed with bufferline
+                bufferline-nvim
+                indent-blankline-nvim # visual indent
+                toggleterm-nvim # better terminal management
+                nvim-treesitter # better code coloring
+                playground # treesitter playground
+                nvim-treesitter-textobjects
+                nvim-treesitter-context # keep current block header (func defn or whatever) on first line
+
+                # Editor Features ####################################
+                vim-abolish # better abbreviations / spelling fixer
+                #vim-surround # most important plugin for quickly handling brackets
+                nvim-surround # .... updated lua-based alternative to tpope's surround
+                vim-unimpaired # bunch of convenient navigation key mappings
+                vim-repeat # supports all of the above so you can use .
+                vim-rsi # brings keyline bindings to editing (like ctrl-e for end of line when in insert mode)
+                vim-visualstar # press * or # on a word to find it
+                kommentary # code commenter
+                nvim-ts-context-commentstring # makes kommentary contextual for embedded languages
+                vim-eunuch # brings cp/mv type commands. :Rename and :Move are particularly handy
+
+                # Autocompletion
+                nvim-cmp # generic autocompleter
+                cmp-nvim-lsp # use lsp as source for completions
+                cmp-nvim-lua # makes vim config editing better with completions
+                cmp-buffer # any text in open buffers
+                cmp-path # complete paths
+                cmp-cmdline # completing in :commands
+                cmp-emoji # complete :emojis:
+                nvim-autopairs # balances parens as you type
+                vim-emoji # TODO: redundant now?
+                luasnip # snippets driver
+                cmp_luasnip # snippets completion
+                friendly-snippets # actual library of snippets used by luasnip
+
+                # Notes
+                # 2022-08-30 I have quite liked taskwiki and vim-roam-task, but both use a #ab12ff
+                # style of tagging tasks that confuses the hell out of markdown editors
+                # that are tag aware. As I'm using NotePlan now to collect tasks, I'm
+                # removing this. 
+                #vim-roam-task # a clone of taskwiki that doesn't require vimwiki
+                zk-nvim # lsp for a folder of notes for searching/linking/etc.
+                true-zen-nvim # distraction free, width constrained writing mode
+                twilight-nvim # dim text outside of current scope
+
+                # Misc
+                vim-fugitive # git management
+                project-nvim
+                vim-tmux-navigator # navigate vim and tmux panes together
+                FixCursorHold-nvim # remove this when neovim #12587 is resolved
+                impatient-nvim # speeds startup times by caching lua bytecode
+                which-key-nvim
+                direnv-vim # auto-execute nix direnv setups
+              ];
+              opt = with pkgs.vimPlugins; [
+                # grammar check
+                vim-grammarous
+              ];
+            };
           };
         };
-        apps.pwnvim = flake-utils.lib.mkApp { 
+        apps.pwnvim = flake-utils.lib.mkApp {
           drv = packages.pwnvim;
-          exePath = "${pkgs.nvim}/bin/nvim";
+          name = "pwnvim";
+          exePath = "/bin/nvim";
         };
-        defaultPackage = pkgs.neovim;
-        defaultApp."${system}" = apps.pwnvim;
-        defaultPackages = packages;
+        packages.default = packages.pwnvim;
+        apps.default = apps.pwnvim;
       }
     );
 
