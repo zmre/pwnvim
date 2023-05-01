8d3b2fcb7d7f710a33848ad3e3738b34ecc5d48a
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2023-05-01 14:07:54 -0600

    fix deprecated bufferline option

--- pwnvim/plugins/bufferline.lua
+++ pwnvim/plugins/bufferline.lua
@@ -1,10 +1,10 @@
 require 'bufferline'.setup {
   options = {
-    numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-    close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+    numbers = "none",                    -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+    close_command = "Bdelete! %d",       -- can be a string | function, see "Mouse actions"
     right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+    left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
+    middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
     indicator = {
       style = "icon",
       icon = "▎",
@@ -36,14 +36,21 @@ require 'bufferline'.setup {
     show_buffer_close_icons = true,
     show_close_icon = true,
     show_buffer_icons = not SimpleUI,
-    show_buffer_default_icon = not SimpleUI,
+    get_element_icon = function(elem)
+      if SimpleUI then
+        return "", ""
+      else
+        local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(elem.filetype, { default = false })
+        return icon, hl
+      end
+    end,
     color_icons = not SimpleUI,
     buffer_close_icon = SimpleUI and "x" or "",
     show_tab_indicators = true,
     persist_buffer_sort = false, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = "thick", -- | "thick" | "thin" | { 'any', 'any' },
+    separator_style = "thick",    -- | "thick" | "thin" | { 'any', 'any' },
     enforce_regular_tabs = false, -- if true, all tabs same width
     always_show_bufferline = true
   },
