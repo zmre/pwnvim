9d3e981463fdce6c8b3e11fccdc0a5e4b164951c
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2023-09-06 17:43:36 -0600

    Make better use of cache

--- update.sh
+++ update.sh
@@ -3,5 +3,9 @@
 OLDRESULT=$(realpath result)
 nix flake update
 nix build
+nix flake archive --json \
+  | jq -r '.path,(.inputs|to_entries[].value.path)' \
+  | cachix push zmre
 echo "Diff with $OLDRESULT"
 nix store diff-closures "$OLDRESULT" ./result |grep -Ev '[∅ε]' |grep '→'
+
