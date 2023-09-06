b93b3801942e4add150fc54082726ba511ab1d88
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2023-09-06 17:43:50 -0600

    Fix a command line q: issue with treesitter (1/2)

--- pwnvim/plugins/treesitter.lua
+++ pwnvim/plugins/treesitter.lua
@@ -6,7 +6,14 @@ require 'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false
   },
   indent = { enable = true, disable = { "yaml" } },
-  incremental_selection = { enable = true },
+  incremental_selection = {
+    enable = true,
+    is_supported = function()
+      -- disable in command window
+      local mode = vim.api.nvim_get_mode().mode
+      return mode ~= "c"
+    end
+  },
   context_commentstring = {
     enable = true,
   },
