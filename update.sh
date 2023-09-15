13920d8a4b21a1bca2c5c2cc0afceee6f863b613
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2023-09-14 22:50:02 -0600

    Reduce amount of cachix caching

--- update.sh
+++ update.sh
@@ -2,9 +2,8 @@
 
 OLDRESULT=$(realpath result)
 nix flake update
-nix build
-nix flake archive --json \
-  | jq -r '.path,(.inputs|to_entries[].value.path)' \
+nix build --json \
+  | jq -r '.[].outputs | to_entries[].value' \
   | cachix push zmre
 echo "Diff with $OLDRESULT"
 nix store diff-closures "$OLDRESULT" ./result |grep -Ev '[∅ε]' |grep '→'
