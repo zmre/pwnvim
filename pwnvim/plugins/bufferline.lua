fd21407bb212dbe11ff7b648e393a4a10ee11429
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2023-09-15 21:34:41 -0600

    Hide fugitive from buffer line

--- pwnvim/plugins/bufferline.lua
+++ pwnvim/plugins/bufferline.lua
@@ -1,10 +1,14 @@
-require 'bufferline'.setup {
+local bufferline = require('bufferline')
+bufferline.setup {
+  ---@diagnostic disable: missing-fields
   options = {
-    numbers = "none",                    -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-    close_command = "Bdelete! %d",       -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
+    mode = "buffers",
+    style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
+    numbers = "none",                               -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+    close_command = "Bdelete! %d",                  -- can be a string | function, see "Mouse actions"
+    right_mouse_command = "Bdelete! %d",            -- can be a string | function, see "Mouse actions"
+    left_mouse_command = "buffer %d",               -- can be a string | function, see "Mouse actions"
+    middle_mouse_command = nil,                     -- can be a string | function, see "Mouse actions"
     indicator = {
       style = "icon",
       icon = "▎",
@@ -12,26 +16,19 @@ require 'bufferline'.setup {
     -- buffer_close_icon = '',
     modified_icon = "●",
     close_icon = SimpleUI and "x" or "",
-    -- close_icon = '',
-    -- hover doesn't work in tmux
-    -- hover = {
-    --   enabled = true,
-    --   delay = 200,
-    --   reveal = { 'close' }
-    -- },
     left_trunc_marker = SimpleUI and "⬅️" or "",
     right_trunc_marker = SimpleUI and "➡️" or "",
     max_name_length = 40,
-    max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+    max_prefix_length = 30,   -- prefix used when a buffer is de-duplicated
     tab_size = 20,
-    -- name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
-    --   -- remove extension from markdown files for example
-    --   if buf.name:match('%.md') then
-    --     return vim.fn.fnamemodify(buf.name, ':t:r')
-    --   end
-    -- end,
-    diagnostics = false, -- | "nvim_lsp" | "coc",
+    diagnostics = "nvim_lsp", -- | "coc",
     diagnostics_update_in_insert = false,
+    custom_filter = function(buf_number, buf_numbers)
+      if vim.bo[buf_number].filetype ~= "fugitive" then
+        return true
+      end
+      return false
+    end,
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     show_buffer_close_icons = true,
     show_close_icon = true,
